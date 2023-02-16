f46bc955dbd2783176fb5fc9d4a7ec7523e143c7
Boots <hello@boots.media>
2023-02-16 14:54:20 -0600

    add react player, add portfolio files

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
 import Button from '@components/Button'
 import Modal from '@components/Modal'
 import Popup from '@components/Popup'
@@ -26,51 +27,62 @@ const Intro = () => {
   }, [])
 
   return (
-    <div className={styles.container}>
-      <div className={clsx(styles.text, richStyles.richText)}>
-        <h1 className={styles.heading}>Hello!</h1>
-        <p>
-          My name is John Highland, but most people know me as <strong>&ldquo;Boots&rdquo;</strong>{' '}
-          &mdash; long story. I currently live in Dallas, TX, with my very loving and understanding
-          wife and two <s>terrorists</s> sons. For {spanWeb} years, I’ve been developing websites,
-          but my first passion was playing bass guitar ({thisYear} marks {spanBass} years). Welcome
-          to my home. Take a look around... make yourself comfy.{' '}
-          <Link href="/contact">Let me know</Link> if you need anything.
-        </p>
-        <p className={styles.links}>
-          <Link href="/bio">
-            <Button className={styles.button} variant="primary" isLink={true}>
-              Get to know me
-            </Button>
-          </Link>
-
-          <Modal
-            open={open}
-            onOpenChange={() => {
-              setOpen(!open)
-            }}
-            trigger={
-              <Button variant="primary" className={styles.button}>
-                Open the modal window
+    <>
+      <div className={styles.container}>
+        <div className={clsx(styles.text, richStyles.richText)}>
+          <h1 className={styles.heading}>Hello!</h1>
+          <p>
+            My name is John Highland, but most people know me as{' '}
+            <strong>&ldquo;Boots&rdquo;</strong> &mdash; long story. I currently live in Dallas, TX,
+            with my very loving and understanding wife and two <s>terrorists</s> sons. For {spanWeb}{' '}
+            years, I’ve been developing websites, but my first passion was playing bass guitar (
+            {thisYear} marks {spanBass} years). Welcome to my home. Take a look around... make
+            yourself comfy. <Link href="/contact">Let me know</Link> if you need anything.
+          </p>
+          <p className={styles.links}>
+            <Link href="/bio">
+              <Button className={styles.button} variant="primary" isLink={true}>
+                Get to know me
               </Button>
-            }
-            size="small"
-          >
-            <Popup />
-          </Modal>
-        </p>
+            </Link>
+
+            <Modal
+              open={open}
+              onOpenChange={() => {
+                setOpen(!open)
+              }}
+              trigger={
+                <Button variant="primary" className={styles.button}>
+                  Open the modal window
+                </Button>
+              }
+              size="small"
+            >
+              <Popup />
+            </Modal>
+          </p>
+        </div>
+        <div className={styles.image}>
+          <Image
+            src={photo}
+            alt="Photo of Boots, sitting in front of a backdrop of cartoon unicorns."
+            placeholder="blur"
+            width="500"
+            height="500"
+            loading="eager"
+          />
+        </div>
       </div>
-      <div className={styles.image}>
-        <Image
-          src={photo}
-          alt="Photo of Boots, sitting in front of a backdrop of cartoon unicorns."
-          placeholder="blur"
-          width="500"
-          height="500"
-          loading="eager"
+
+      {/* <div className={styles.player}>
+        <ReactPlayer
+          url="/images/work/video/mizzen-new-year-2023-web.mp4"
+          width="100%"
+          height="100%"
+          controls={true}
         />
-      </div>
-    </div>
+      </div> */}
+    </>
   )
 }
 
