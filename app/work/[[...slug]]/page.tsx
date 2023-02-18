5b082a112ce592f3089492accb3176421ad5bc37
Boots <hello@boots.media>
2023-02-17 19:34:23 -0600

    update work data file, project cards initial state complete

--- app/work/[[...slug]]/page.tsx
+++ app/work/[[...slug]]/page.tsx
@@ -19,16 +19,18 @@ type WorkProps = {
 
 const Work = ({ params }: WorkProps) => {
   const { slug } = params
-  const activeSection = slug ? sections.find(item => item.id === slug[0]) : null
+  const activeSection = slug ? sections.find(item => item.id === slug[0]) : sections[0]
   let filteredProjects = projects
 
-  if (activeSection?.id !== undefined) {
+  if (activeSection?.id !== 'all') {
     filteredProjects = projects.filter(proj => proj.category === activeSection?.id)
   }
 
   return (
     <>
-      {activeSection?.description && <p>{activeSection?.description}</p>}
+      {activeSection?.description && (
+        <p dangerouslySetInnerHTML={{ __html: activeSection?.description }} />
+      )}
       <div className={styles.projects}>
         {filteredProjects.length ? (
           filteredProjects.map(proj => <Project key={proj.id} project={proj} />)
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -9,32 +9,62 @@ type ProjectProps = {
     title: string
     client: string
     year: string
+    projectType: string
     category: string
     agency: string
     logo: string
     screenshot: string
     url: string
     media: string
+    mediaType: string
     skills: string[]
     notes: string
   }
 }
 
 const Project = ({ project }: ProjectProps) => {
+  const {
+    title,
+    client,
+    year,
+    projectType,
+    category,
+    agency,
+    logo,
+    screenshot,
+    url,
+    media,
+    mediaType,
+    skills,
+    notes,
+  } = project
+
+  const ctaLabel =
+    projectType === 'Song'
+      ? 'Listen Now'
+      : projectType.includes('Video')
+      ? 'Watch Now'
+      : projectType === 'photo'
+      ? 'View Photo'
+      : 'More Info'
+
   return (
-    <div className={styles.container} data-category={project.category}>
-      <h2 className={styles.heading}>{project.title}</h2>
+    <div className={styles.container} data-category={category.replace('-', ' ')}>
+      <h2 className={styles.heading}>
+        {title}{' '}
+        <span>
+          {projectType} &bull; {year}
+        </span>
+      </h2>
       <div className={styles.logo}>
-        <Image src={project.logo} height={100} width={100} alt={`${project.client} Logo`} />
+        <Image src={logo} height={100} width={100} alt={`${client} Logo`} />
       </div>
       <div className={styles.content}>
         <div className={styles.screenshot}>
-          <Image
-            src={project.screenshot}
-            height={225}
-            width={400}
-            alt={`${project.title} Screenshot`}
-          />
+          <Image src={screenshot} height={225} width={400} alt={`${title} Screenshot`} />
+        </div>
+        <div className={styles.cta}>
+          <button>{ctaLabel}</button>
         </div>
       </div>
     </div>
