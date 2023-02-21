e4bc9d407bb874b29d2551437df61cb5e009f65a
Boots <hello@boots.media>
2023-02-21 01:46:40 -0600

    add ga tag

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import { SITE_URL } from '@/utils/constants'
 
 import DefaultTags from './default-tags'
@@ -5,7 +7,7 @@ import DefaultTags from './default-tags'
 const Head = () => {
   const title = 'Web Development, Bass Guitar, and Much More - John "Boots" Highland'
   const description =
-    'The personal landing page for John "Boots" Highland - a Dallas-based bass guitarist, web developer, husband and father.'
+    'The online home for John "Boots" Highland - a Dallas-based bass guitarist, web developer, husband and father.'
   const url = ''
 
   return (
@@ -23,6 +25,24 @@ const Head = () => {
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:url" content={`${SITE_URL}${url}`} />
+
+      {/* GOOGLE ANALYTICS */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QXQK9E9F9V"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QXQK9E9F9V');
+          `,
+        }}
+      />
     </>
   )
 }
